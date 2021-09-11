b29549c3cacee19973e67b9eb23af39efa2071d0
✦✧✧νιиϲєиzο✧✧✦ <90239565+vincenzo-op@users.noreply.github.com>
2021-09-11 21:05:09 +0530

    Update vincenzo.py

--- vincenzo.py
+++ vincenzo.py
@@ -1482,11 +1482,10 @@ async def ping(e):
 @dav.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
 @raj.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
 @put.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
-#async def ___(e):
-    usage = "restarts bot... type .restart"
-    if e.sender_id in SMEX_USERS:
-        text = "𝙍𝙚𝙨𝙩𝙖𝙧𝙩𝙚𝙙\n\nPlease wait till it reboots..."
-        await e.reply(text, parse_mode=None, link_preview=None )
+ async def ___(e):
+      if e.sender_id in SMEX_USERS:
+      text = "𝙍𝙚𝙨𝙩𝙖𝙧𝙩𝙚𝙙\n\nPlease wait till it reboots..."
+      await e.reply(text, parse_mode=None, link_preview=None )
         try:
             await idk.disconnect()
         except Exception as e:
