ed241c2cd8d1afdf2fe00ff2d75eb465e4e327a6
✦✧✧νιиϲєиzο✧✧✦ <90239565+vincenzo-op@users.noreply.github.com>
2021-09-11 10:33:03 +0530

    Update vincenzo.py

--- vincenzo.py
+++ vincenzo.py
@@ -1456,133 +1456,16 @@ async def ping(e):
         end = datetime.now()
         ms = (end-start).microseconds / 1000
         await event.edit(f"🇵 🇴 🇳 🇬 !\n`{ms}` 🔰🔰\n ᴠɪɴᴄᴇɴᴢᴏ ᴍᴜʟᴛɪ sᴘᴀᴍ ʙᴏᴛ ⎝╰‿╯⎠ "
-###
-##                         ###
-from telethon.tl import functions
-from telethon.tl.functions.channels import GetFullChannelRequest
-from telethon.tl.functions.messages import GetFullChatRequest
+###vincenzo_op
+##  kang with credits else mc,bc and gay                       
 
-async def get_chatinfo(event):
-    chat = event.text[10:]
-    chat_info = None
-    if chat:
-        try:
-            chat = int(chat)
-        except ValueError:
-            pass
-    if not chat:
-        if event.reply_to_msg_id:
-            replied_msg = await event.get_reply_message()
-            if replied_msg.fwd_from and replied_msg.fwd_from.channel_id is not None:
-                chat = replied_msg.fwd_from.channel_id
-        else:
-            chat = event.chat_id
-    try:
-        chat_info = await event.client(GetFullChatRequest(chat))
-    except:
-        try:
-            chat_info = await event.client(GetFullChannelRequest(chat))
-        except ChannelInvalidError:
-            await event.reply("`Invalid channel/group`")
-            return None
-        except ChannelPrivateError:
-            await event.reply(
-                "`This is a private channel/group or I am banned from there`"
-            )
-            return None
-        except ChannelPublicGroupNaError:
-            await event.reply("`Channel or supergroup doesn't exist`")
-            return None
-        except (TypeError, ValueError):
-            await event.reply("`Invalid channel/group`")
-            return None
-    return chat_info
-
-
-def user_full_name(user):
-    names = [user.first_name, user.last_name]
-    names = [i for i in list(names) if i]
-    full_name = " ".join(names)
-    return full_name
-
-                         
-            
-@idk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@ydk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@wdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@hdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@sdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@adk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@bdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@cdk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@edk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@ddk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@vkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@kkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@lkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@mkk.on(events.NewMessage(incoming=True, pattern=r"\.inviteall")) 
-@sid.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@shy.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@aan.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@ake.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@eel.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@khu.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@shi.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@yaa.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@dav.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@raj.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-@put.on(events.NewMessage(incoming=True, pattern=r"\.inviteall"))
-async def get_users(event):
-  sender = await event.get_sender()
-  me = await event.client.get_me()
-  if event.sender_id in SMEX_USERS:
-    lo_l = event.text[10:]
-    vincenzo = await event.reply("`Processing.....`")
-    if not lo_l:
-        return await vincenzo.edit("Give Channel")
-    if lo_l == "@our_SECRET_SOCIETY":
-        return await hell.edit("Restricted to invite users from there.")
-    elif lo_l == "@our_SECRET_SOCIETY":
-        return await vincenzo.edit("Restricted to invite users from there.")
-    elif lo_l == "@our_SECRET_SOCIETY":
-        return await vincenzo.edit("Restricted to invite users from there.")
-    phuck = await get_chatinfo(event)
-    chat = await event.get_chat()
-    if event.is_private:
-        return await vincenzo.edit("`Sorry, Cant add users here`")
-    s = 0
-    f = 0
-    error = "None"
-
-    await vincenzo.edit("**INVITING USERS !!**")
-    async for user in event.client.iter_participants(phuck.full_chat.id):
-        try:
-            if error.startswith("Too"):
-                return await vincenzo.edit(
-                    f"**INVITING FINISHED !**\n\n**Error :** \n`{error}`\n\n**Invited :**  `{s}` users. \n**Failed to Invite :** `{f}` users."
-                )
-            await event.client(
-                functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
-            )
-            s = s + 1
-            await vincenzo.edit(
-                f"**INVITING USERS.. **\n\n**Invited :**  `{s}` users \n**Failed to Invite :**  `{f}` users.\n\n**×Error :**  `{error}`"
-            )
-        except Exception as e:
-            error = str(e)
-            f = f + 1
-    return await hell.edit(
-        f"**INVITING FINISHED** \n\n**Invited :**  `{s}` users \n**Failed :**  `{f}` users."
-    )
-  else:
-   return await event.reply("`phuck....dont scrap too much...`")
 # _______
 
 @idk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
 @ydk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
 @wdk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
 @hdk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
-@sdk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))                                                      
 @adk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
 @bdk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
 @cdk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
